e45630113e50390cd04928d25d059b7e936072f7
MonamiYP <monami.yoshioka@gmail.com>
2022-12-14 23:41:07 +0100

    Include game loop files into main

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,17 @@
-#include <iostream>
-#include <SDL2/SDL.h>
-#include <SDL2/SDL_image.h>
+#include "Game.hpp"
+
+Game* game = nullptr;
 
 int main(int argc, const char* argv[]) {
-    while(true) {
+    game = new Game();
 
+    game->init("Unmasked", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, false);
+
+    while(game->running()) {
+        game->handleEvents();
+        game->update();
+        game->render();
     }
+
+    game->clean();
 }
