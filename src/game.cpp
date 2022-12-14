643c594e7ca1ecd3aabc0b538138afa9e8e332c3
MonamiYP <monami.yoshioka@gmail.com>
2022-12-14 23:14:36 +0100

    Initialising SDL

--- src/game.cpp
+++ src/game.cpp
@@ -9,7 +9,25 @@ Game::~Game() {
 }
 
 void Game::init(const char* title, int xpos, int ypos, int width, int height, bool full_screen) {
+    int flags = 0;
+    if(full_screen)
+        flags = SDL_WINDOW_FULLSCREEN;
 
+    if(SDL_Init(SDL_INIT_EVERYTHING) == 0) {
+        std::cout << "SDL_Init successful" << std::endl;
+
+        window = SDL_CreateWindow(title, xpos, ypos, width, height, flags);
+        if(window)
+            std::cout << "Window created successfully" << std::endl;
+        
+        renderer = SDL_CreateRenderer(window, -1, 0);
+        if(renderer)
+            std::cout << "Renderer created successfully" << std::endl;
+        
+        is_running = true;
+    } else {
+        is_running = false;
+    }
 }
 
 void Game::handleEvents() {
